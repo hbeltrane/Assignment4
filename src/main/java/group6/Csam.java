d599cbb69983ab976649f43eea85999b0a7695a8
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-03-25 09:23:04 -0400

    Added Java Bean and DAO

---
+++ src/main/java/group6/Csam.java
@@ -0,0 +1,41 @@
+package group6;
+
+public class Csam {
+    public Csam() {
+    // TODO Auto-generated constructor stub
+    }
+    String sid,sName,grade;
+    double gpa;
+    String getSid()
+    {
+        return sid;
+    }
+    String getSName()
+    {
+        return sName;
+    }
+    String getGrade()
+    {
+        return grade;
+    }
+    double getGpa()
+    {
+        return gpa;
+    }
+    void setSid(String sid)
+    {
+        this.sid=sid;
+    }
+    void setSName(String sName)
+    {
+        this.sName = sName;
+    }
+    void setGrade(String grade)
+    {
+        this.grade = grade;
+    }
+    void setGpa(double gpa)
+    {
+        this.gpa = gpa;
+    }
+}
---
+++ src/main/java/group6/CsamDAO.java
@@ -0,0 +1,104 @@
+package group6;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+public class CsamDAO {
+    public CsamDAO() {
+        // TODO Auto-generated constructor stub
+    }
+    public static Connection getConnection(){
+        Connection con=null;
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:oradb","scott","tiger");
+            System.out.print("in jdbc");
+        }catch(Exception e){System.out.println(e);}
+        return con;
+    }
+    public static int save(Csam e){
+        int status=0;
+        try{
+            Connection con = getConnection();
+            System.out.println("after connection done");
+            PreparedStatement ps=con.prepareStatement(
+                    "insert into lambton(sid,sname,gpa,grade) values (?,?,?,?)");
+            System.out.println("after insert statement");
+            ps.setString(1,e.getSid());
+            ps.setString(2,e.getSName());
+            ps.setDouble(3,e.getGpa());
+            ps.setString(4,e.getGrade());
+            status=ps.executeUpdate();
+            con.close();
+        }catch(Exception ex){ex.printStackTrace();}
+        return status;
+    }
+    public static Csam getStudentById(String id){
+        Csam e=new Csam ();
+        try{
+            Connection con= getConnection();
+            PreparedStatement ps=con.prepareStatement("select * from lambton where sid=?");
+            ps.setString(1,id);
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                e.setSName(rs.getString(2));
+                e.setGpa(Double.parseDouble(rs.getString(3)));
+                e.setGrade(rs.getString(4));
+            }
+            con.close();
+        }catch(Exception ex){ex.printStackTrace();}
+        return e;
+    }
+    public static int update(Csam p){
+        int status=0;
+        try{
+            Connection con = getConnection();
+            String sql="update lambton set sname='"+p.getSName()+"',gpa="+p.getGpa()+",grade='"+p.getGrade()+"' where sid='"+p.getSid()+"'";
+            System.out.println(sql);
+            PreparedStatement ps=con.prepareStatement(sql);
+            status=ps.executeUpdate();
+            con.close();
+        }catch(Exception ex){ex.printStackTrace();
+        }
+        return 0;
+    }
+    public static int delete(String sid){
+        int status=0;
+        try{
+            Connection con=getConnection();
+            String sql="delete from lambton where sid='"+sid+"'";
+            System.out.println(sql);
+            PreparedStatement ps=con.prepareStatement(sql);
+            status=ps.executeUpdate();
+            con.close();
+        }catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return status;
+    }
+    /* public Emp getEmpById(int id){
+    String sql="select * from Emp99 where id=?";
+    return template.queryForObject(sql, new Object[]{id},new BeanPropertyRowMapper<Emp>(Emp.class));
+    } */
+    public static List<Csam> getAllStudents(){
+        List<Csam> list=new ArrayList<Csam>();
+        try{
+            Connection con=getConnection();
+            System.out.println("Inside get all students");
+            PreparedStatement ps=con.prepareStatement("select * from lambton");
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                Csam e=new Csam();
+                e.setSid(rs.getString(1));
+                e.setSName(rs.getString(2));
+                e.setGrade(rs.getString(4));
+                e.setGpa(Double.parseDouble(rs.getString(3)));
+                list.add(e);
+            }
+            con.close();
+        }catch(Exception e){e.printStackTrace();
+        }
+        return list;
+    }
+}
